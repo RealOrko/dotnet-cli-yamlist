ab9cab67cb4d709f04a94564500d70b7bcad972b
RealOrko <realorko@nowhere.com>
2020-06-28 00:03:39 +0100

    More testing

--- visualise.sh
+++ visualise.sh
@@ -4,7 +4,7 @@ set -ex
 set -o pipefail
 
 dotnet publish src/yamlist/ -o build/
-cp ../delivery-azure-pipelines/ci/deploy_enterprise_archive.yml build/d.yml
+cp ../offline_cs_enterprise_archive/ci/deploy.yml ./build/d.yml
 pushd ./build
 ./yi json -f d.yml > j.json
 ./yi yaml -f j.json > y.yml
