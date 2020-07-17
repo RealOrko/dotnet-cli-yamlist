6149d1faad77960d16461ad02986ee43df7e654a
RealOrko <realorko@nowhere.com>
2020-07-17 20:09:10 +0100

    Started scaffolding a local environment with a simple pipeline to test changes

--- src/yamlist/Commands/AddTaskCommand.cs
+++ src/yamlist/Commands/AddTaskCommand.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using yamlist.Modules.Commands;
 using yamlist.Modules.Commands.Attributes;
 using yamlist.Modules.IO;
+using yamlist.Modules.IO.Json.Model;
 
 namespace yamlist.Commands
 {
@@ -19,14 +22,36 @@ namespace yamlist.Commands
         public int Execute(AddTaskArguments args)
         {
             var input = File.ReadAllText(args.InputFile);
-            var concourse = Converter.ToConcourse(input, args.InputFile, args.Debug);
-            
-            
+            var pipeline = Converter.ToConcourse(input, args.InputFile, args.Debug);
+
+            var job = FindOrCreateJob(pipeline, args.JobName, args.TaskName, args.TasksFolder);
             
             Console.WriteLine("Wheeee!");
             
             return 0;
         }
 
+        private object FindOrCreateJob(Pipeline pipeline, string jobName, string taskName, string taskFolder)
+        {
+            var newOrExistingjob = pipeline.Jobs.FirstOrDefault(x => x.Name == jobName);
+            if (newOrExistingjob == null)
+            {
+                return new Job()
+                {
+                    Name = jobName,
+                    Plan = new List<JobPlan>()
+                    {
+                        new JobPlan()
+                        {
+                            Task = taskName,
+                            File = Path.Combine(taskFolder, $"{taskName}.yml"),
+                            Image = "ubuntu"
+                        }
+                    }
+                };
+            }
+
+            return newOrExistingjob;
+        }
     }
 }
