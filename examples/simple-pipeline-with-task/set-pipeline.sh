2418d23bac173d953d8709d8805fa88ce20e2f3d
RealOrko <realorko@nowhere.com>
2020-07-19 15:18:01 +0100

    More testing ...

--- examples/simple-pipeline-with-task/set-pipeline.sh
+++ examples/simple-pipeline-with-task/set-pipeline.sh
@@ -9,4 +9,4 @@ fly -t local-dev sync
 fly -t local-dev validate-pipeline -c ./pipeline.yml
 fly -t local-dev set-pipeline -p local-dev -c ./pipeline.yml
 fly -t local-dev unpause-pipeline -p local-dev
-fly -t local-dev trigger-job -j local-dev/my-simple-job
+fly -t local-dev trigger-job -j local-dev/my-simple-job-with-task
