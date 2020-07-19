7bd552d333f6c0c5397ee4734ac7d562a53cd992
RealOrko <realorko@nowhere.com>
2020-07-19 14:31:41 +0100

    Added some more utility scripts

---
+++ examples/simple-pipeline-with-task/intercept.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Needs to be run with sudo privs on fedora 32, not assuming this works on osx-64 at all. 
+
+# Please run 'docker-compose up -d' from a fresh clone of https://github.com/RealOrko/concourse-docker first.
+
+fly --target local-dev login -n main -c http://localhost:8080/ -u test -p test
+fly -t local-dev intercept -j local-dev/my-simple-job
